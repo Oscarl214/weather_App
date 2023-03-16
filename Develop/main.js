8c920039e489d9e1890333aef812ef2f2c957982
Oscar Leal <oscarleal234@gmail.com>
2023-03-15 23:25:36 -0500

    pulling my weather data

--- Develop/main.js
+++ Develop/main.js
@@ -1,27 +1,33 @@
-var cityInfo=$("#cityData");
-var searches=$(".pastSearches");
+var cityInfo = $("#cityData");
+var searches = $(".pastSearches");
 
-var userCity=$("#userCity");
+var userCity = $("#userCity");
 
+var apiKey = "48774715b5f5fb8a452c6ba72a2e6d98"; //my API key
 
+var city = ""; //empty variable to match Users input
+var queryURL =
+  "http://api.openweathermap.org/data/2.5/weather?q=" +
+  city +
+  "&appid=" +
+  apiKey;
+//TODO: Figure out how to fetch information needed
 
-var apiKey= "48774715b5f5fb8a452c6ba72a2e6d98";
-var city="";
-var queryURL= "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+var attempt2Url =
+  "https://api.openweathermap.org/data/2.5/weather?q=Dallas&appid=" +
+  apiKey +
+  "&units=imperial";
 
-var formSubmitHandler = function (event) {
-    event.preventDefault();
-  
-    var city = userCity.value.trim();
-  
-    if (city) {
-      getUserRepos(city);
-  
-      cityInfo.textContent = '';
-      userCity.value = '';
-    } else {
-      alert('Please enter a valid city');
-    }
-  };
+fetch(attempt2Url, {
+  // The browser fetches the resource from the remote server without first looking in the cache.
+  // The browser will then update the cache with the downloaded resource.
+  cache: "reload",
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
 
-  //TODO: Figure out how to fetch information needed
+var displayCity = function () {};
