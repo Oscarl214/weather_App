26a8dc179dcef94c61db8ca463b15577fe331c53
Oscar2145 <74516013+Oscar2145@users.noreply.github.com>
2023-03-16 15:31:14 -0500

    added designated spots for wind,humidity, and tem

--- Develop/main.js
+++ Develop/main.js
@@ -1,8 +1,13 @@
-var cityInfo = $("#cityData");
-var searches = $(".pastSearches");
-var userFormEl = $("#user-form"); //text input element
-var cityInputEl = $("#userCity"); //users search
-var userSearchEl = $("#userSearchBtn"); //submit button
+let cityInfo = $("#cityData"); //Div containing City data info
+let searches = $(".pastSearches"); //Div where old searches will be displayed that are stored once User searches for them
+let userFormEl = $("#user-form"); //text input element
+let cityInputEl = $("#userCity"); //users search
+let userSearchBtn = $("#userSearchBtn"); //submit button
+const cityName=$("#city"); //grabs my h1 where I want the city that is search up to display
+const cityTemp=$("#temp"); //grabs my span element where I want the temp data to display
+const cityWind=$("#wind"); //grabs my span element where I want the wind data to display
+const cityHumid=$("#humid"); //grabs my span element where I want the humidity to display
+
 var apiKey = "48774715b5f5fb8a452c6ba72a2e6d98"; //my API key
 
 var city = ""; //empty variable to match Users input
@@ -25,3 +30,7 @@ fetch(attempt2Url, {
   .then(function (data) {
     console.log(data);
   });
+
+  
+
+
